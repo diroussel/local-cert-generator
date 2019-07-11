80284ddb2d25a8524341a18db6686e4b14152f53
Dave Roussel <Dave.Roussel@lloydsbanking.com>
2019-07-11 14:39:30 +0100

    chef updates

--- createSelfSigned.sh
+++ createSelfSigned.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
+declare TARGET="chef"
+openssl req -new -sha256 -nodes -out $TARGET.csr -newkey rsa:2048 -keyout server.key -config $TARGET.csr.cnf
+openssl x509 -req -in $TARGET.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out $TARGET.crt -days 500 -sha256 -extfile $TARGET.v3.ext
